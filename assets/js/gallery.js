42bc93e2bcfbec3922feb7522767bcaaa2c512ae
RitualEffect <ritualeffectband@gmail.com>
2022-10-15 15:19:33 +0000

    Add custom javascript file for image carousels and link to new gallery page

---
+++ assets/js/gallery.js
@@ -0,0 +1,16 @@
+// Wait for the DOM to finish loading before running
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Set initial active image and image counters
+
+    // Add event listeners to thumbnail buttons
+
+    // Add event listeners to 'next' buttons
+
+    // Add event listeners to 'previous' buttons
+
+    /* Add touch event listeners to image carousels for swiping between images. */
+});
+
+// Event handler functions
